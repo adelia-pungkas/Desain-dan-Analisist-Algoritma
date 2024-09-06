a96e735c280e96e0d97f36494adbb33a6e2be1d0
adelia-pungkas <adelia.pungkasaritaufik@student.upj.ac.id>
2024-09-06 15:13:23 +0700

    Add files via upload

---
+++ Adelia Pungkasari Taufik DAA pertemuan-1.py
@@ -0,0 +1,8 @@
+
+angkaA = float(input("Masukkan angka pertama: "))
+angkaB = float(input("Masukkan angka kedua: "))
+
+if angkaA > angkaB:
+    print("Angka terbesar adalah:", angkaA)
+else:
+    print("Angka terbesar adalah:", angkaB)
